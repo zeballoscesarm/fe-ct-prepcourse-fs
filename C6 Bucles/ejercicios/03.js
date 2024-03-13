4d156166f2a9e0386bb900945e756c9d860a42c8
Cesar <zeballoscesarm@gmail.com>
2024-03-13 11:39:20 -0300

    modulo 6 hasta el ejercicio 10

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,12 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if (x>y){
+    return x;
+  }
+  else{
+    return y;
+  }
 }
 
 module.exports = obtenerMayor;
--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,13 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  var numStr=num.toString();
+  if (numStr.length===3){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 module.exports = tieneTresDigitos;
